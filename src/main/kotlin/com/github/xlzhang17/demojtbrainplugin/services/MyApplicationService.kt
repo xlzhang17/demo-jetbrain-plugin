1d21080fe0078f7bfd975db528fc3e22dfe92d65
GitHub Action <action@github.com>
2021-07-30 03:33:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xlzhang17/demojtbrainplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xlzhang17.demojtbrainplugin.services
+
+import com.github.xlzhang17.demojtbrainplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/xlzhang17/demojtbrainplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xlzhang17/demojtbrainplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xlzhang17.demojtbrainplugin.services
 
+import com.github.xlzhang17.demojtbrainplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
